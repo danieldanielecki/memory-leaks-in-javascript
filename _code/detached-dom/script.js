1916b21d6defea8dd2b2556e022226a9a0147c64
✅ Daniel Danielecki <daniel.danielecki@mail.com>
2025-03-09 16:08:35 +0300

    fix: improve detached DOM example

--- _code/detached-dom/script.js
+++ _code/detached-dom/script.js
@@ -1,9 +1,13 @@
 let detachedNode;
 function createDetachedNode() {
-  const div = document.createElement("div");
-  document.body.appendChild(div);
-  document.body.removeChild(div); // Removed from DOM
-  detachedNode = div; // Still in memory
+  const divElement = document.createElement("div");
+  divElement.style.padding = "10px";
+  divElement.style.margin = "5px";
+  divElement.style.height = "50px";
+  divElement.style.background = "red";
+  document.body.appendChild(divElement);
+  // document.body.removeChild(divElement); // Removed from DOM
+  detachedNode = divElement; // Still in memory
   console.log("detachedNode before setting to null: ", detachedNode);
 }
 createDetachedNode();
