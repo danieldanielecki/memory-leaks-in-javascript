f349e4e728c143f75108fb5e9122929269819779
✅ Daniel Danielecki <daniel.danielecki@mail.com>
2025-03-14 15:05:36 +0100

    feat: highlighting LoCs

--- _code/closures/script2.js
+++ _code/closures/script2.js
@@ -2,11 +2,10 @@ function outerFunction() {
   let largeArray = new Array(1000000).fill("memory leak");
 
   // Use the required value, not the entire object.
-  // const importantValue = largeObject[0];
+  const importantValue = largeArray[0];
 
   return function innerFunction() {
-    console.log(largeArray);
-    // console.log(importantValue); // only this is kept in the memory, garbage collector will now free memory from largeArray.
+    console.log(importantValue); // only this is kept in the memory, garbage collector will now free memory from largeArray.
   };
 }
 const leak = outerFunction();
